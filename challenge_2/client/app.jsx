c9cf97b4a710699f3bd39f3822dcd6554e7c7310
emvh <emilyvho@gmail.com>
2020-07-31 00:52:39 -0700

    use chartJS to display time-series chart

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,51 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ''
+    }
+    this.chartRef = React.createRef();
+  }
 
+  getData() {
+    axios({
+      method: 'get',
+      url: '/bitcoin'
+    })
+    .then((response) => {
+      const data = {
+        labels: Object.keys(response.data.bpi),
+        datasets: [{
+          label: '$',
+          borderColor: 'rgb(255, 99, 132)',
+          fill: false,
+          data: Object.values(response.data.bpi)
+        }]
+      }
+      this.setState({ data });
+    })
+    .catch((error) => console.log(error))
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    const { data, labels } = this.state;
+    console.log('data state', this.state.data)
+    return (
+      <div>
+        <Line
+          data={data}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios')
 
+app.use(express.static('public'))
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/bitcoin', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-01-01&end=2013-01-30')
+  .then((response) => {
+    res.status(200).send(response.data.bpi);
+  })
+  .catch((err) => res.status(404).send(err))
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
