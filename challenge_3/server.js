4127d12741012c8f0dacafc23b3eee170f6a7f43
emvh <emilyvho@gmail.com>
2020-08-02 01:04:08 -0700

    Setup

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('public'))
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
